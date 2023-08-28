f3ef9c7b9a8cb415c887303bb2c2421eaac1a503
Eduardo Teixeira <131905631+EduardoTeixeiraS@users.noreply.github.com>
2023-08-28 11:58:21 -0300

    Funções TypeScript
    Funções criadas com TypeScript

---
+++ exercicio_ts.ts
@@ -0,0 +1,9 @@
+let multiplicaNums2 = (num1: number, num2: number): number => num1 * num2;
+
+console.log(multiplicaNums2(5, 5))
+
+let saudacao = (nome: string) => {
+    console.log(`Olá ${nome}!`)
+}
+
+saudacao('Eduardo')
